3edb58ebb02fd8cb38e42842db95b1ed912c6896
Kyon Prom <8162402@isd535.org>
2021-09-16 09:41:02 -0500

    added x and y for ball

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,23 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext ("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillstyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+let x = canvas.width/2;
+let y = canvas.height-30;
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle= "green";
-ctx.fill();
-ctx.closePath();
+let dx = 2;
+let dy = -2;
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath(); 
+function draw(){
+    // draw the ball
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+setInterval(draw, 10);
+
+// change the x and y values for the ball
+x+= dx;
+y+= dy;
 
