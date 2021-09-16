78fa9b3d3bf2e61c001371ad47408179cf7909e8
Kyon Prom <8162402@isd535.org>
2021-09-16 08:48:51 -0500

    re added rectangle stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext ("2d");
-
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillstyle = "#FF0000";
@@ -18,3 +17,14 @@ ctx.rect(160, 10, 100, 40);
 ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
 ctx.stroke();
 ctx.closePath(); 
+
+function draw(){
+    // drawing code
+    // draw the ball
+    ctx.beginPath();
+    ctx.arc(50, 50, 10, 0, Math.PI*2);
+    ctx.fillStyle = "0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+setInterval(draw, 10);
